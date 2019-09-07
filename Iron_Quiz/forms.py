dfd5a99e826aa46de12d333aa0439cfe500d3a1f
Stefano <stefanopav93@gmail.com>
2019-09-07 11:21:21 +0200

    added placeholders for inputs

--- Iron_Quiz/forms.py
+++ Iron_Quiz/forms.py
@@ -4,11 +4,12 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', [validators.Length(min=1)])
+    username = StringField('Username', [validators.Length(min=1)], render_kw={"placeholder": "Your username"})
     submit = SubmitField('Register or Sign In')
 
 
 class QuestionsForm(FlaskForm):
-    question = StringField('Question', [validators.Length(min=1)])
-    right_answer = StringField('Right answer', [validators.Length(min=1)])
-    wrong_answer = StringField('Wrong answer', [validators.Length(min=1)])
+    question = StringField('Question', [validators.Length(min=1)], render_kw={"placeholder": "Your question"})
+    right_answer = StringField('Right answer', [validators.Length(min=1)], render_kw={"placeholder": "Right answer"})
+    wrong_answer = StringField('Wrong answer', [validators.Length(min=1)], render_kw={"placeholder": "Wrong answer"})
+    submit = SubmitField('Submit question')
