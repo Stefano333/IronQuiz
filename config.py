9c8688adec7f9b5f559a8f02fca951ff49d03617
Stefano <stefanopav93@gmail.com>
2019-09-07 09:04:40 +0200

    added configuration file

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from flask_migrate import Migrate
+basedir = os.path.abspath(os.path.dirname(__file__))
+sqlite_dir = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-word'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or sqlite_dir
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
