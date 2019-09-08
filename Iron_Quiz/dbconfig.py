cf88a7b378100dcd927510534429f64c1e4dba40
Stefano <stefanopav93@gmail.com>
2019-09-08 15:15:40 +0200

    user added in database if not existing

--- Iron_Quiz/dbconfig.py
+++ Iron_Quiz/dbconfig.py
@@ -1,5 +1,6 @@
 import os
 import mysql.connector as mariadb
+from mysql.connector import errorcode
 
 config = {
     'user': 'ste',
@@ -10,19 +11,34 @@ config = {
 }
 
 
-def get_query(query: str) -> dict:
+def get_user(user_to_search: str) -> bool:
+    get_username_query = '''
+        SELECT username
+        FROM users
+        WHERE username = '{0}' '''.format(user_to_search)
+    insert_username_query = '''
+        INSERT INTO users(username)
+        VALUES('{0}')'''.format(user_to_search)
+
     try:
         db = mariadb.connect(**config)
-        cursor = db.cursor()
-        return cursor.execute(query)
+        cursor = db.cursor(buffered=True)
+        print('search {0}'.format(user_to_search))
+        cursor.execute(get_username_query)
 
-    except mysql.connector.Error as err:
+        if not cursor.rowcount:
+            print('insert {0}'.format(user_to_search))
+            cursor.execute(insert_username_query)
+            db.commit()
+
+    except mariadb.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
             print("Something is wrong with your user name or password")
         elif err.errno == errorcode.ER_BAD_DB_ERROR:
             print("Database does not exist")
         else:
             print(err)
+            print('ope')
 
 
 if __name__ == "__main__":
@@ -31,11 +47,40 @@ if __name__ == "__main__":
     db_password = os.environ.get('DB_PASSWORD')
     db_name = os.environ.get('DB_NAME')
 
+    print('''
+    db_host: {0}
+    db_user: {1}
+    db password: {2}
+    db name: {3}
+    '''.format(db_host, db_user, db_password, db_name))
+
     try:
         db = mariadb.connect(**config)
         print("connected to database {0}".format(config['database']))
 
-    except mysql.connector.Error as err:
+        cursor = db.cursor(buffered=True)
+        query = ('''
+        SELECT * FROM users WHERE username='imperialsoldier3'
+        ''')
+
+        records = {}
+
+        # cursor.execute(query)
+        cursor.execute("INSERT INTO users(username) VALUES('ciao')")
+        db.commit()
+        print(cursor.rowcount)
+
+        for id, username in cursor:
+            records[id] = username
+            print("{0}: {1}".format(id, username))
+
+        if(records):
+            for id in records.keys():
+                print("{0}: {1}".format(id, records[id]))
+        else:
+            print("no records")
+
+    except mariadb.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
             print("Something is wrong with your user name or password")
         elif err.errno == errorcode.ER_BAD_DB_ERROR:
--- Iron_Quiz/routes.py
+++ Iron_Quiz/routes.py
@@ -1,8 +1,10 @@
 from Iron_Quiz import app
 from flask import render_template, request, flash, redirect, url_for
 from Iron_Quiz.forms import LoginForm, QuestionsForm
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
+from Iron_Quiz.dbconfig import get_user
+
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -16,6 +18,7 @@ def login():
                 flash('admin')
                 return redirect(url_for('admin'))
             else:
+                get_user(username)
                 return redirect(url_for('quiz'))
     return render_template('login.html', form=login_form)
 
@@ -24,6 +27,7 @@ def login():
 def admin():
     question_form = QuestionsForm()
     return render_template('admin.html', question_form=question_form)
+
 
 @app.route('/quiz')
 def quiz():
