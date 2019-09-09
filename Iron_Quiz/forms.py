0699baae72febf5883789f6c7b49921b83857db1
Stefano <stefanopav93@gmail.com>
2019-09-09 20:31:30 +0200

    added form for user's quiz

--- Iron_Quiz/forms.py
+++ Iron_Quiz/forms.py
@@ -4,12 +4,22 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', [validators.Length(min=1)], render_kw={"placeholder": "Your username"})
+    username = StringField('Username', [validators.Length(min=1)], render_kw={
+                           "placeholder": "Your username"})
     submit = SubmitField('Register or Sign In')
 
 
 class QuestionsForm(FlaskForm):
-    question = StringField('Question', [validators.Length(min=1)], render_kw={"placeholder": "Your question"})
-    right_answer = StringField('Right answer', [validators.Length(min=1)], render_kw={"placeholder": "Right answer"})
-    wrong_answer = StringField('Wrong answer', [validators.Length(min=1)], render_kw={"placeholder": "Wrong answer"})
+    question = StringField('Question', [validators.Length(min=1)], render_kw={
+                           "placeholder": "Your question"})
+    right_answer = StringField('Right answer', [validators.Length(
+        min=1)], render_kw={"placeholder": "Right answer"})
+    wrong_answer = StringField('Wrong answer', [validators.Length(
+        min=1)], render_kw={"placeholder": "Wrong answer"})
     submit = SubmitField('Submit question')
+
+
+class AnswersForm(FlaskForm):
+    answer = StringField('Answer', [validators.Length(min=1)], render_kw={
+                         "placeholder": "Your answer..."})
+    submit = SubmitField('Submit answer')
