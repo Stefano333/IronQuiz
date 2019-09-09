9b5bdec7ecde12037dd4c02a9dc19fe4f22cf72f
Stefano <stefanopav93@gmail.com>
2019-09-09 20:35:03 +0200

    added functions for queries

--- Iron_Quiz/dbconfig.py
+++ Iron_Quiz/dbconfig.py
@@ -15,19 +15,17 @@ def get_user(user_to_search: str) -> bool:
     get_username_query = '''
         SELECT username
         FROM users
-        WHERE username = '{0}' '''.format(user_to_search)
+        WHERE username = '{0}';'''.format(user_to_search)
     insert_username_query = '''
         INSERT INTO users(username)
-        VALUES('{0}')'''.format(user_to_search)
+        VALUES('{0}');'''.format(user_to_search)
 
     try:
         db = mariadb.connect(**config)
         cursor = db.cursor(buffered=True)
-        print('search {0}'.format(user_to_search))
         cursor.execute(get_username_query)
 
         if not cursor.rowcount:
-            print('insert {0}'.format(user_to_search))
             cursor.execute(insert_username_query)
             db.commit()
 
@@ -38,7 +36,119 @@ def get_user(user_to_search: str) -> bool:
             print("Database does not exist")
         else:
             print(err)
-            print('ope')
+    finally:
+        cursor.close()
+        db.close()
+
+
+def insert_new_question(question: str, right_answer: str, wrong_answer: str) -> bool:
+    insert_new_question_query = '''
+    INSERT INTO questions(question, right_answer, wrong_answer) 
+    VALUES('{0}','{1}','{2}')
+    '''.format(question, right_answer, wrong_answer)
+
+    try:
+        db = mariadb.connect(**config)
+        cursor = db.cursor()
+        cursor.execute(insert_new_question_query)
+        db.commit()
+
+        return True
+
+    except mariadb.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+            return False
+
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+            return False
+
+        else:
+            print(err)
+            return False
+    finally:
+        cursor.close()
+        db.close()
+
+
+def get_current_question() -> dict:
+    query = '''
+    SELECT id, question, right_answer, wrong_answer
+    FROM questions
+    WHERE answered = FALSE
+    LIMIT 1
+    '''
+
+    question_data = {}
+
+    try:
+        db = mariadb.connect(**config)
+        cursor = db.cursor(buffered=True)
+        cursor.execute(query)
+
+        if cursor.rowcount:
+            for id, question, right_answer, wrong_answer in cursor:
+                question_data['id'] = id
+                question_data['question'] = question
+                question_data['right_answer'] = right_answer
+                question_data['wrong_answer'] = wrong_answer
+
+        return question_data
+
+    except mariadb.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+    finally:
+        cursor.close()
+        db.close()
+
+
+def book_answer(username: str, question_id: int) -> bool:
+    get_placement_query = '''
+    SELECT MAX(placement) AS max_placement
+    FROM answering_queue
+    WHERE answering_queue_questions_id = {0}
+    '''.format(question_id)
+    placement = 1
+
+    try:
+        db = mariadb.connect(**config)
+        cursor = db.cursor(buffered=True)
+        cursor.execute(get_placement_query)
+
+        if cursor.rowcount:
+            for max_placement in cursor:
+                print(max_placement[0])
+                if max_placement[0] == None:
+                    placement = 1
+                else:
+                    placement += max_placement[0]
+
+        book_answer_query = '''
+            INSERT INTO answering_queue(placement, answering_queue_questions_id, answering_queue_users_username)
+            VALUES('{0}', '{1}', '{2}')
+            '''.format(placement, question_id, username)
+
+        cursor.execute(book_answer_query)
+        db.commit()
+
+        return True
+
+    except mariadb.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+    finally:
+        cursor.close()
+        db.close()
 
 
 if __name__ == "__main__":
@@ -65,7 +175,6 @@ if __name__ == "__main__":
 
         records = {}
 
-        # cursor.execute(query)
         cursor.execute("INSERT INTO users(username) VALUES('ciao')")
         db.commit()
         print(cursor.rowcount)
