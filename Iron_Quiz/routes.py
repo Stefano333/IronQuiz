15b035a27e16f0597a5a63d995d8ab1b1c7bb6ed
Stefano <stefanopav93@gmail.com>
2019-09-09 20:30:42 +0200

    added features for showing actual question

--- Iron_Quiz/routes.py
+++ Iron_Quiz/routes.py
@@ -1,21 +1,35 @@
 from Iron_Quiz import app
-from flask import render_template, request, flash, redirect, url_for
-from Iron_Quiz.forms import LoginForm, QuestionsForm
+from flask import render_template, request, flash, redirect, url_for, session
+from Iron_Quiz.forms import LoginForm, QuestionsForm, AnswersForm
 # from flask_sqlalchemy import SQLAlchemy
 # from flask_migrate import Migrate
-from Iron_Quiz.dbconfig import get_user
+from Iron_Quiz.dbconfig import get_user, get_current_question, insert_new_question, book_answer
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
+def home():
+    logged_in = session.get('logged_in')
+    logged_user = session.get('username')
+
+    if not logged_in:
+        return redirect(url_for('login'))
+    elif logged_in and logged_user:
+        if logged_user == 'admin':
+            return redirect(url_for('admin'))
+        else:
+            return redirect(url_for('quiz'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     login_form = LoginForm()
 
     if request.method == 'POST':
         if login_form.validate_on_submit():
-            username = login_form.username.data
-            print(username)
+            session['logged_in'] = True
+            username = session['username'] = login_form.username.data
+
             if username == 'admin':
-                flash('admin')
                 return redirect(url_for('admin'))
             else:
                 get_user(username)
@@ -23,13 +37,29 @@ def login():
     return render_template('login.html', form=login_form)
 
 
-@app.route('/admin')
+@app.route('/admin', methods=['GET', 'POST'])
 def admin():
+    current_question = get_current_question()
     question_form = QuestionsForm()
+
+    if request.method == 'POST':
+        if question_form.validate_on_submit():
+            insert_new_question(question_form.question.data,
+                                question_form.right_answer.data, question_form.wrong_answer.data)
+
+            return redirect(url_for('admin'))
+
+    if current_question:
+        return render_template('admin.html', current_question=current_question)
     return render_template('admin.html', question_form=question_form)
 
 
-@app.route('/quiz')
+@app.route('/quiz', methods=['GET', 'POST'])
 def quiz():
-    flash('user')
+    current_question_id = get_current_question()['id']
+    logged_user = session.get('username')
+
+    if request.method == 'POST':
+        book_answer(logged_user, current_question_id)
+
     return render_template('quiz.html')
